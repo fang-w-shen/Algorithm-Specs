ed909180480ed16ace50aa4a842e5be3243fd4ee
fang-w-shen <fang-wen.shen@tuftandneedle.com>
2022-02-02 00:28:14 -0500

    Graphs

--- specs/graph/graph.test.js
+++ specs/graph/graph.test.js
@@ -5,14 +5,14 @@
 
 /*
   parameters:
-  myId                - number    - the id of the user who is the root node
+  myId                - number    - the id of the user who is the root id
   
   degreesOfSeparation - number   - how many degrees of separation away to look on the graph
 */
 
 /*
   getUser  - function - a function that returns a user's object given an ID
-
+  
   example
 
   {
@@ -27,11 +27,45 @@ const { getUser } = require("./jobs");
 
 const findMostCommonTitle = (myId, degreesOfSeparation) => {
   // code goes here
+  let queue = [myId]
+  let keys = new Set([myId])
+  let mostCommonJob = {}
+  while (degreesOfSeparation + 1) {
+    degreesOfSeparation--
+    let compared = queue.length
+    while (queue.length && compared) {
+      compared--
+      let user = getUser(queue.shift())
+      mostCommonJob[user.title] = mostCommonJob[user.title] ? mostCommonJob[user.title] += 1 : 1
+      let length = user.connections.length
+      while (length) {
+        length--
+        let connection = user.connections[length]
+        if (!keys.has(connection)) {
+          queue.push(connection)
+          keys.add(connection)
+        }
+      }
+    }
+  }
+
+  let titles = Object.keys(mostCommonJob)
+  let mostJobCount = 0
+  let mostJobTitle
+  for (let i = 0; i < titles.length - 1; i++) {
+    let currentTitle = titles[i]
+    if (mostCommonJob[currentTitle] > mostJobCount) {
+      mostJobTitle = currentTitle
+      mostJobCount = mostCommonJob[currentTitle]
+    }
+  }
+  return mostJobTitle
+
 };
 
 // unit tests
 // do not modify the below code
-test.skip("findMostCommonTitle", function () {
+describe("findMostCommonTitle", function () {
   // the getUser function and data comes from this CodePen: https://codepen.io/btholt/pen/NXJGwa?editors=0010
   test("user 30 with 2 degrees of separation", () => {
     expect(findMostCommonTitle(30, 2)).toBe("Librarian");
@@ -48,7 +82,7 @@ test.skip("findMostCommonTitle", function () {
   });
 });
 
-test.skip("extra credit", function () {
+describe("extra credit", function () {
   test("user 1 with 7 degrees of separation – this will traverse every user that's followed by someone else. five users are unfollowed", () => {
     expect(findMostCommonTitle(1, 7)).toBe("Geological Engineer");
   });
