2912c0fa48c9924dc9cdd00835a710f5c2007e1f
fang-w-shen <fang-wen.shen@tuftandneedle.com>
2022-02-02 23:50:35 -0500

    PathfindingBreadthFirstTraversal

--- specs/pathfinding/pathfinding.test.js
+++ specs/pathfinding/pathfinding.test.js
@@ -18,15 +18,88 @@ const logMaze = require("./logger");
 
 function findShortestPathLength(maze, [xA, yA], [xB, yB]) {
   // code goes here
+  const visited = maze.map((row, y) =>
+    row.map((origin, x) => ({
+      closed: origin === 1,
+      iteration: 0,
+      openedBy: 0,
+      x,
+      y
+    }))
+  )
+  visited[yA][xA].openedBy = "A"
+  visited[yB][xB].openedBy = "B";
+  let aq = [visited[yA][xA]]
+  let bq = [visited[yB][xB]]
+  let found = false
+  let steps = 0
+  while (aq.length && bq.length) {
+    steps++
+    let aNeighbors = []
+    let bNeighbors = []
+    // USE AQUEUE TO GET ANEIGHBORS
+
+    while (aq.length) {
+      const coord = aq.shift()
+      aNeighbors.push(...getNeighbors(visited, coord.x, coord.y))
+    }
+    // USE NEW ANEIGHBORS TO REFILL AQUEUE
+
+    while (!found && aNeighbors.length) {
+      let neighbor = aNeighbors.shift()
+      if (neighbor && neighbor.openedBy == "B") {
+        return neighbor.iteration + steps
+
+      }
+      else if (neighbor && neighbor.openedBy == 0) {
+        neighbor.openedBy = "A"
+        neighbor.iteration = steps
+        aq.push(neighbor)
+      }
+
+    }
+    // USE BQUEUE TO GET BNEIGHBORS
+    while (bq.length) {
+      const coord = bq.shift()
+      bNeighbors.push(...getNeighbors(visited, coord.x, coord.y))
+      // console.log(aNeighbors)
+    }
+    // USE NEW BNEIGHBORS TO REFILL BQUEUE
+    while (!found && bNeighbors.length) {
+      let neighbor = bNeighbors.shift()
+      if (neighbor && neighbor.openedBy == "A") {
+        return neighbor.iteration + steps
+      }
+      else if (neighbor && neighbor.openedBy == 0) {
+        neighbor.openedBy = "B"
+        neighbor.iteration = steps
+        bq.push(neighbor)
+      }
+
+    }
+  }
+  return -1
 }
 
+function getNeighbors(node, x, y) {
+  let left = right = down = up = null
+  if (x - 1 >= 0 && !node[y][x - 1].closed)
+    left = node[y][x - 1]
+  if (y - 1 >= 0 && !node[y - 1][x].closed)
+    up = node[y - 1][x]
+  if (x + 1 <= node[0].length - 1 && !node[y][x + 1].closed)
+    right = node[y][x + 1]
+  if (y + 1 <= node.length - 1 && !node[y + 1][x].closed)
+    down = node[y + 1][x]
+  return [left, right, up, down]
+}
 // there is a visualization tool in the completed exercise
 // it requires you to shape your objects like I did
 // see the notes there if you want to use it
 
 // unit tests
 // do not modify the below code
-describe.skip("pathfinding – happy path", function () {
+describe("pathfinding – happy path", function () {
   const fourByFour = [
     [2, 0, 0, 0],
     [0, 0, 0, 0],
@@ -90,7 +163,7 @@ describe.skip("pathfinding – happy path", function () {
 // I care far less if you solve these
 // nonetheless, if you're having fun, solve some of the edge cases too!
 // just remove the .skip from describe.skip
-describe.skip("pathfinding – edge cases", function () {
+describe("pathfinding – edge cases", function () {
   const byEachOther = [
     [0, 0, 0, 0, 0],
     [0, 2, 2, 0, 0],
